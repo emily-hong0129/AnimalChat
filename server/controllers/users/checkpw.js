5f7b7cd99410d34e618ca75e0a96e08e80d96962
emily-hong0129 <85302678+emily-hong0129@users.noreply.github.com>
2021-12-20 23:09:40 +0900

    chore : update files

---
+++ server/controllers/users/checkpw.js
@@ -0,0 +1,31 @@
+const { user } = require("../../models")
+const { isAuthorized } = require("../tokenFunc")
+const { decrypto } = require("../users/setpw")
+
+module.exports = async (req, res) => {
+    const accessTokenData = isAuthorized(req)
+
+    if (!accessTokenData) {
+        res.status(401).send("로그인 정보를 확인해주세요.")
+    }
+
+    const { user_id, nickname } = accessTokenData
+    const { password } = req.body
+
+    user.findOne({
+        where: {
+            user_id: user_id,
+            nickname: nickname,
+        },
+    }).then((data) => {
+        const depw = decrypto(data.password)
+        if (!data) {
+            res.send("확인되지 않은 사용자입니다.")
+        }
+        if (depw !== password) {
+            res.send("비밀번호가 맞지 않습니다.")
+        }
+        delete data.dataValues.password
+        res.status(201).send("비밀번호 확인 완료")
+    })
+}
--- server/controllers/users/edituserinfo.js
+++ server/controllers/users/edituserinfo.js
@@ -2,28 +2,20 @@ const { user } = require("../../models")
 const { animal } = require("../../models")
 const { generateAccessToken, generateRefreshToken } = require("../tokenFunc")
 
-module.exports = async(req, res) => {
-	//console.log("반려동물추가 : ", req.body);
-	// 반려동물 추가
-	const animal_photo = req.body.animal_photo
-	const {userId, animalName, animalYob, selectType} = req.body.animalInfo
-	console.log(userId, animalName, animalYob, selectType, animal_photo);
+module.exports = async (req, res) => {
+    const animal_photo = req.body.animal_photo
+    const { userId, animalName, animalYob, selectType } = req.body.animalInfo
 
-	if(
-		!userId ||
-		!animalName ||
-		!animalYob ||
-		!selectType
-	){
-		res.status(401).send("정보를 모두 기입해야합니다.")
-	}else{
-		await animal.create({
-			userId: userId,
-			animaltype: selectType,
-			animalname: animalName,
-			animalyob: animalYob,
-			animal_photo: animal_photo,
-		})
-		res.status(201).send({message: "ok"})
-	}
-};
+    if (!userId || !animalName || !animalYob || !selectType) {
+        res.status(401).send("정보를 모두 기입해야합니다.")
+    } else {
+        await animal.create({
+            userId: userId,
+            animaltype: selectType,
+            animalname: animalName,
+            animalyob: animalYob,
+            animal_photo: animal_photo,
+        })
+        res.status(201).send({ message: "ok" })
+    }
+}
